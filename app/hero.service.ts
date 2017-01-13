4e527625eaf60d05d6f85615f4d162db689e3632
prichards-jacada <prichards@jacada.com>
2017-01-13 13:40:57 +0000

    Delete a hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -34,6 +34,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
                .toPromise()
